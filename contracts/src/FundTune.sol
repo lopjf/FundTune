0430f7af03b6c6ff4dbdebddca9679f96fba3018
lopjf <loris.penelon@outlook.com>
2024-03-29 20:24:37 +0000

    contract creates and manage event with USDT. Tests to be written

--- contracts/src/FundTune.sol
+++ contracts/src/FundTune.sol
@@ -1,5 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.19;
+import "../node_modules/@openzeppelin/contracts/access/Ownable.sol";
+import "../node_modules/@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 /**
  * @title FundTune
@@ -8,29 +10,103 @@ pragma solidity ^0.8.19;
  * @dev 
 */
 
-contract FundTune {
+contract FundTune is Ownable{
 
     // Custom events
 
+    event EventCreated(
+        uint256 indexed eventId,
+        string eventName,
+        string eventDescription,
+        string artist,
+        string location,
+        uint256 contributionAmount
+    );
+
+    event Participated(
+        uint256 indexed eventId,
+        address indexed participant,
+        uint256 contributionAmount
+    );
+
     /* ========== STATE VARIABLES ========== */
+    uint256 public eventCount = 0;
     struct Event {
         uint256 id;
         string eventName;
         string eventDescription;
         string artist;
         string location;
-        uint256 contributionAmount;
-        address[] participants;
+        uint256 pricePerTicketUSDT;
         uint256 totalParticipants;
         uint256 totalContribution;
+        uint256 withdrawnAmount;
+        bool active;
     }
-    mapping(uint256 => Event) public events;
+    Event[] public events;
+    mapping(uint256 => mapping(address => uint256)) public participantContribution;
+    address public usdtAddress;
 
     /* ========== CONSTRUCTOR ========== */
-    constructor() {
+
+    constructor(address _usdtAddress) Ownable(msg.sender) {
+        usdtAddress = _usdtAddress;
+    }
+    
+    /* ========== SETTER FUNCTIONS ========== */
+
+    function createEvent(
+        string memory _eventName,
+        string memory _eventDescription,
+        string memory _artist,
+        string memory _location,
+        uint256 _pricePerTicketUSDT
+    ) public onlyOwner {
+        events.push(
+            Event({
+                id: eventCount,
+                eventName: _eventName,
+                eventDescription: _eventDescription,
+                artist: _artist,
+                location: _location,
+                pricePerTicketUSDT: _pricePerTicketUSDT,
+                totalParticipants: 0,
+                totalContribution: 0,
+                withdrawnAmount: 0,
+                active: true
+            })
+        );
+        eventCount++;
+        emit EventCreated(eventCount, _eventName, _eventDescription, _artist, _location, _pricePerTicketUSDT);
     }
 
-    /* ========== VIEW FUNCTIONS ========== */
+    function participate(uint256 _eventId, uint256 _contributionAmountUSDT) public {
+        require(events[_eventId].active, "Not an active Event");
+        require(_contributionAmountUSDT % events[_eventId].pricePerTicketUSDT == 0, "Not a valid contribution amount");
 
-    /* ========== SETTER FUNCTIONS ========== */
+        // this require approving USDT from the USDT contract. IERC20(usdtAddress).approve(fundTuneContractAddress, contributionAmountUSDT);
+        require(IERC20(usdtAddress).transferFrom(msg.sender, address(this), _contributionAmountUSDT), "Transfer failed");
+
+        participantContribution[_eventId][msg.sender] += _contributionAmountUSDT;
+        events[_eventId].totalContribution += _contributionAmountUSDT;
+        events[_eventId].totalParticipants = events[_eventId].totalContribution / events[_eventId].pricePerTicketUSDT;
+        emit Participated(_eventId, msg.sender, _contributionAmountUSDT);
+    }
+
+    function disableEvent(uint256 _eventId) public onlyOwner {
+        require(events[_eventId].active == true, "Event is already inactive");
+        events[_eventId].active = false;
+    }
+
+    function enableEvent(uint256 _eventId) public onlyOwner {
+        require(events[_eventId].active == false, "Event is already active");
+        events[_eventId].active = true;
+    }
+
+    function withdraw(uint256 _eventId) public onlyOwner {
+        require(events[_eventId].active == false, "Event is still active");
+        require(events[_eventId].totalContribution > events[_eventId].withdrawnAmount, "Nothing to withdraw");
+        require(IERC20(usdtAddress).transfer(owner(), events[_eventId].totalContribution - events[_eventId].withdrawnAmount), "Transfer failed");
+        events[_eventId].withdrawnAmount += events[_eventId].totalContribution;
+    }
 }
--- contracts/test/FundTune.t.sol
+++ contracts/test/FundTune.t.sol
@@ -4,21 +4,23 @@ pragma solidity ^0.8.13;
 import {Test, console} from "forge-std/Test.sol";
 import {FundTune} from "../src/FundTune.sol";
 
+    // Maybe use forge test --fork-url <your_rpc_url> to test with a forked mainnet and use USDT from mainnet
+
 contract FundTuneTest is Test {
     FundTune public fundTune;
 
-    // function setUp() public {
-    //     fundTune = new FundTune();
-    //     fundTune.setNumber(0);
-    // }
+    function setUp() public {
+        fundTune = new FundTune(address(0));
+    }
 
-    // function test_Increment() public {
-    //     fundTune.increment();
-    //     assertEq(fundTune.number(), 1);
-    // }
+    function testCreateEvent() public {
+        fundTune.createEvent("Event1", "Event1 Description", "Artist1", "Location1", 100);
+        assertEq(fundTune.eventCount(), 1);
+    }
 
-    // function testFuzz_SetNumber(uint256 x) public {
-    //     fundTune.setNumber(x);
-    //     assertEq(fundTune.number(), x);
-    // }
+    function testParticipate() public {
+        fundTune.createEvent("Event1", "Event1 Description", "Artist1", "Location1", 100);
+        fundTune.participate(0, 100);
+        assertEq(fundTune.events(0).totalParticipants, 1);
+    }
 }
