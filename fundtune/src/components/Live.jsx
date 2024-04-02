21259228687496ca703527b1b1f58216a14f034a
Daniel B <daarf92@gmail.com>
2024-04-02 08:42:07 +0100

    live page started, card flex fixed

--- fundtune/src/components/Live.jsx
+++ fundtune/src/components/Live.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import Card from './Card';
 
 const Live = () => {
   return (
-    <div>Live</div>
+    <div id="live">
+      <div id="live-title">Live Funds</div>
+      
+      <div id="card-cont">
+        <Card />
+        <Card />  
+        <Card />  
+        <Card />  
+        <Card />  
+
+      </div>  
+
+      <div id="new-fund">New Fund</div>
+    </div>
   )
 }
 
